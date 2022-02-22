d61a77428497ca51335f711c7f28882a0cd4583a
themartto <themartto@gmail.com>
2022-02-22 12:43:24 +0200

    example

--- lib.rs
+++ lib.rs
@@ -18,11 +18,25 @@ mod foo {
                 value
             }
         }
-
+        
+        // won't compile 
+        // error[E0599]: no function or associated item named `test` found for struct `Foo` in the current scope
+        // #[cfg(feature = "ink-debug")]
+        // #[ink(message)]
+        // pub fn test(&self) {
+        //     debug_println!("yeah")
+        // }
+        
+        
+        // cfg!(feature = "ink-debug") will always return false no matter if --release flag is set or not
         #[ink(message)]
-        #[cfg(feature = "ink-debug")]
-        pub fn test(&self) {
-            debug_println!("yeah")
+        pub fn test2(&self) {
+            if cfg!(feature = "ink-debug") {
+                debug_println!("DEBUG mode")    
+            } else {
+                debug_println!("RELEASE mode ")
+            }
+            
         }
     }
 }
